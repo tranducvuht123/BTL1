2aef5458ef92c71b039de552dbe8dab7abc9e7cd
tranducvuht123 <92667456+tranducvuht123@users.noreply.github.com>
2021-11-23 22:48:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function test() {
+    let hoten = prompt("Nhập họ tên:");
+    alert("Bạn đã nhập họ tên" + hoten);
+
+    let gmail = prompt("Nhập gmail:");
+    alert("Bạn đã gmail" + gmail);
+
+    if (hoten == null) {
+        alert("0")
+    }
+}
